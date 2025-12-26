9ac98bb9c5585e42958c8a41902734a601a917b8
hastirahpey <hastirahpey@gmail.com>
2025-12-26 16:37:11 +0330

    project structure and fastapi setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
 
 app = FastAPI(title="GPU Service Simulator")
+
+# ساخت جدول‌ها (فعلاً خالی است ولی مهم است)
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def root():
     return {"message": "GPU Service API is running"}
+
 
 @app.get("/health")
 def health_check():
